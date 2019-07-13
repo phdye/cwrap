95bcfa2e9d426e1e8abed628593a8e01831b65ed
philip-h-dye <philip@phd-solutions.com>
2019-07-13 13:36:46 -0400

    Run test suite with:  python setup.py test

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from distutils.core import setup
+    
+# from distutils.core import setup
+from setuptools import setup
 
 
 setup(name='CWrap',
@@ -8,5 +10,10 @@ setup(name='CWrap',
                 'cwrap.frontends.gccxml',
                 'cwrap.frontends.clang','cwrap.frontends.clang.clang',
                 ],
-      scripts=["bin/cwrap"]
+      scripts=["bin/cwrap"],
+      setup_requires=["Cython", "pytest-runner"],
+      tests_require=["pytest"],
+      # zip_safe needs to be false for pxd files to be available to cython cimport
+      #   https://cython.readthedocs.io/en/latest/src/userguide/sharing_declarations.html
+      zip_safe = False,
       )
